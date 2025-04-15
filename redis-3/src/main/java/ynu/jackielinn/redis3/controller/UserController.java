0564765ac97e076de0afb24eb8e12c8532e5c0e2
JackieLin2004 <linxy20040304@163.com>
2025-04-16 01:35:59 +0800

    part 3

---
+++ redis-3/src/main/java/ynu/jackielinn/redis3/controller/UserController.java
@@ -0,0 +1,46 @@
+package ynu.jackielinn.redis3.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
+import ynu.jackielinn.redis3.entity.User;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    private static final String USER_KEY_PREFIX = "user:";
+
+    @Autowired
+    public UserController(RedisTemplate<String, Object> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    @PostMapping("/users")
+    public String createUser(@RequestBody User user) {
+        redisTemplate.opsForValue().set(USER_KEY_PREFIX + user.getId(), user);
+        return "User created: " + user.getId();
+    }
+
+    @GetMapping("/users/{id}")
+    public User getUser(@PathVariable String id) {
+        return (User) redisTemplate.opsForValue().get(USER_KEY_PREFIX + id);
+    }
+
+    @PutMapping("/users/{id}")
+    public String updateUser(@PathVariable String id, @RequestBody User user) {
+        if (redisTemplate.hasKey(USER_KEY_PREFIX + id)) {
+            redisTemplate.opsForValue().set(USER_KEY_PREFIX + id, user);
+            return "User updated: " + id;
+        }
+        return "User not found: " + id;
+    }
+
+    @DeleteMapping("/users/{id}")
+    public String deleteUser(@PathVariable String id) {
+        redisTemplate.delete(USER_KEY_PREFIX + id);
+        return "User deleted: " + id;
+    }
+}
---
+++ redis-3/src/test/java/ynu/jackielinn/redis3/Redis3ApplicationTests.java
@@ -0,0 +1,13 @@
+package ynu.jackielinn.redis3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Redis3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
